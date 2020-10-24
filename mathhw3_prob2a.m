7fdcb3950ff565bf85a09fee0d20f966d77e9bd5
ktbartas <70544196+ktbartas@users.noreply.github.com>
2020-10-23 20:25:55 -0700

    phase plots for hw3 number 2

---
+++ mathhw3_prob2a.m
@@ -0,0 +1,35 @@
+clear all; close all; clc;
+% 10/23/20
+% i stole most of this code from here: http://matlab.cheme.cmu.edu/2011/08/09/phase-portraits-of-a-system-of-odes/
+
+%y(1) will be x
+%(y(2) will be y
+f = @(t,Y) [-3*Y(1)+4*Y(2); -2*Y(1)+3*Y(2)];
+
+y1 = linspace(-10,10,20);
+y2 = linspace(-10,10,20);
+
+% creates two matrices one for all the x-values on the grid, and one for
+% all the y-values on the grid. Note that x and y are matrices of the same
+% size and shape, in this case 20 rows and 20 columns
+[x,y] = meshgrid(y1,y2);
+size(x);
+size(y);
+
+u = zeros(size(x));
+v = zeros(size(x));
+
+% we can use a single loop over each element to compute the derivatives at
+% each point (y1, y2)
+t=0; % we want the derivatives at each point at t=0, i.e. the starting time
+for i = 1:numel(x)
+    Yprime = f(t,[x(i); y(i)]);
+    u(i) = Yprime(1);
+    v(i) = Yprime(2);
+end
+
+quiver(x,y,u,v,'r'); figure(gcf)
+xlabel('y_1')
+ylabel('y_2')
+axis tight equal;
+title('Phase plot for Math227A hw#3, problem 2A')
